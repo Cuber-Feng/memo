8bcc4fb29bf07c1d5738e5f88088c340df6c81bc
Feng Yuxiang <2017feng35@gmail.com>
2026-05-10 18:43:25 +0800

    style

--- src/pages/Cube.jsx
+++ src/pages/Cube.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Container } from '@mui/material';
+import { Typography, Container, Box } from '@mui/material';
 import HeroCard from '../components/HeroCard';
 
 export default function Cube() {
@@ -8,14 +8,14 @@ export default function Cube() {
             <Typography variant="h3">Rubik's Cube</Typography>
             <Typography>When you forget the algorithms...</Typography>
             <Container sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'left' }}>
-                <HeroCard title="CFOP" description="119 algorithms" to="/CubeAlg/cfop"/>
-                <HeroCard title="ROUX(OH)" description="CMLL" to="https://sites.google.com/view/kianroux/oh-cmll"/>
-                <HeroCard title="Blindfolded" description="彳亍" to="about:blank"/>
-                <HeroCard title="Big Cubes" description="Pariety, L4E" to="about:blank"/>
-                <HeroCard title="Pyraminx" description="4-Edge Method" to="about:blank"/>
-                <HeroCard title="Megaminx" description="Last Layer" to="about:blank"/>
-                <HeroCard title="Skewb" description="控心法" to="about:blank"/>
-                <HeroCard title="Square-1" description="Lin Method" to="about:blank"/>
+                <HeroCard title="CFOP" description="119 algorithms" to="/CubeAlg/cfop" />
+                <HeroCard title="ROUX(OH)" description="CMLL" to="https://sites.google.com/view/kianroux/oh-cmll" />
+                <HeroCard title="Blindfolded" description="彳亍" to="about:blank" />
+                <HeroCard title="Big Cubes" description="Pariety, L4E" to="about:blank" />
+                <HeroCard title="Pyraminx" description="4-Edge Method" to="about:blank" />
+                <HeroCard title="Megaminx" description="Last Layer" to="about:blank" />
+                <HeroCard title="Skewb" description="控心法" to="about:blank" />
+                <HeroCard title="Square-1" description="Lin Method" to="about:blank" />
             </Container>
         </Container>
     );
--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,34 @@ import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
     palette: {
-        primary: { main: '#1976d2' },
-        secondary: { main: '#f50057' },
+        primary: { main: '#E36A6A' },
+        secondary: { main: '#FFB2B2' },
+        background: {
+            default: '#FFF2D0',
+            paper: '#FFFBF1',
+        }
     },
     typography: {
-        fontFamily: 'Roboto, Arial, sans-serif',
+        fontFamily: '"Roboto", "Arial", sans-serif', // <-- global font
+        h1: { fontFamily: '"Roboto Slab", serif' },  // optional per variant
+        h2: { fontFamily: '"Roboto Slab", serif' },
+        body1: { fontFamily: '"Roboto", sans-serif' },
+        body2: { fontFamily: '"Arial", sans-serif' },
     },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                a: {
+                    color: '#c94747',      // secondary color
+                    textDecoration: 'none', // optional: remove underline
+                    '&:hover': {
+                        textDecoration: 'underline',
+                    },
+                },
+            },
+        },
+    },
+
 })
 
 export default theme
